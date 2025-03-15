8334b837b41ef95a12d5a9b60d2e6198212035b3
Sabastian Leeper <sabastianleeper@u.boisestate.edu>
2025-03-14 18:42:27 -0600

    Various method comment changes.

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -1,11 +1,13 @@
 package fa.nfa;
 
-import fa.FAInterface;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
 
 /**
  * Implements a Nondeterministic Finite Automaton (NFA).
- * author: Parker and Sabastian
+ * More specifically, adds onto DFA by adding epsilon transitions, e-closures and sets of states.
+ * @author Parker and Sabastian
  */
 public class NFA implements fa.nfa.NFAInterface {
     private Set<NFAState> states; // Set of all states in the NFA
@@ -21,7 +23,10 @@ public class NFA implements fa.nfa.NFAInterface {
         this.startState = null;
     }
 
-    // Adds a symbol to the alphabet (ignoring 'e' since it's reserved for epsilon transitions)
+    /**
+     * Adds a symbol to the alphabet (ignoring 'e' since it's reserved for epsilon transitions)
+     * @param symbol the symbol to add to the alphabet
+     */    
     @Override
     public void addSigma(char symbol) {
         if (symbol != 'e') { 
@@ -29,7 +34,11 @@ public class NFA implements fa.nfa.NFAInterface {
         }
     }
 
-    // Adds a state to the NFA
+    /**
+     * Adds a state to the NFA
+     * @param name the name of the state to add
+     * @return true if the state was added successfully, otherwise false.
+     */    
     @Override
     public boolean addState(String name) {
         if (getState(name) != null) return false; // State already exists
@@ -37,7 +46,11 @@ public class NFA implements fa.nfa.NFAInterface {
         return true;
     }
 
-    // Sets the start state of the NFA
+    /**
+     * Sets the start state of the NFA
+     * @param name the name of the state to set as the start state
+     * @return true if the start state was set successfully, otherwise false
+     */    
     @Override
     public boolean setStart(String name) {
         NFAState state = getState(name);
@@ -46,7 +59,11 @@ public class NFA implements fa.nfa.NFAInterface {
         return true;
     }
 
-    // Marks a state as a final (accepting) state
+     /**
+     * Marks a state as a final (accepting) state
+     * @param name the name of the state to mark as final
+     * @return true if the state was marked as final successfully, otherwise false
+     */
     @Override
     public boolean setFinal(String name) {
         NFAState state = getState(name);
@@ -55,7 +72,13 @@ public class NFA implements fa.nfa.NFAInterface {
         return true;
     }
 
-    // Adds a transition from one state to a set of destination states on a given symbol
+    /**
+     * Adds a transition from one state to a set of destination states on a given symbol
+     * @param fromState the name of the state where the transition starts
+     * @param toStates the set of names of the states where the transition ends
+     * @param symbol the symbol on which the transition occurs
+     * @return true if the transition was added successfully, otherwise false
+     */
     @Override
     public boolean addTransition(String fromState, Set<String> toStates, char symbol) {
         NFAState from = getState(fromState);
@@ -74,7 +97,11 @@ public class NFA implements fa.nfa.NFAInterface {
         return true;
     }
 
-    // Retrieves a state by name
+     /**
+     * Retrieves a state by name
+     * @param name the name of the state to retrieve
+     * @return the state with the given name, otherwise null
+     */
     @Override
     public NFAState getState(String name) {
         for (NFAState state : states) {
@@ -83,34 +110,53 @@ public class NFA implements fa.nfa.NFAInterface {
         return null;
     }
 
-    // Checks if a given state is the start state
+    /**
+     * Checks if a given state is the start state
+     * @param name the name of the state to check
+     * @return true if the state is the start state, otherwise false
+     */
     @Override
     public boolean isStart(String name) {
         NFAState state = getState(name);
         return state != null && state == startState;
     }
 
-    // Checks if a given state is a final state
+    /**
+     * Checks if a given state is a final state
+     * @param name the name of the state to check
+     * @return true if the state is a final state, otherwise false
+     */
     @Override
     public boolean isFinal(String name) {
         NFAState state = getState(name);
         return state != null && finalStates.contains(state);
     }
 
-    // Retrieves all states reachable from a given state on a specific symbol
+    /**
+     * Retrieves all states reachable from a given state on a specific symbol
+     * @param state the state from which to retrieve transitions
+     * @param symbol the symbol on which the transitions occur
+     * @return a set of states reachable from the given state on the given symbol
+     */
     @Override
     public Set<NFAState> getToState(NFAState state, char symbol) {
         return state.toStates(symbol);
     }
 
-    // Retrieves the alphabet of the NFA
+     /**
+     * Retrieves the alphabet of the NFA
+     * @return the set of valid input symbols
+     */
     @Override
     public Set<Character> getSigma() {
         return alphabet;
     }
 
-    // Computes the epsilon closure of a state using DFS
-    @Override
+    /**
+     * Computes the epsilon closure of a state using DFS
+     * @param state the state chosen to compute the epsilon closure
+     * @return a set of states reachable from the given state through epsilon transitions (e-closures)
+     */    @Override
     public Set<NFAState> eClosure(NFAState state) {
         Set<NFAState> closure = new HashSet<>();
         Stack<NFAState> stack = new Stack<>();
@@ -128,7 +174,11 @@ public class NFA implements fa.nfa.NFAInterface {
         return closure;
     }
 
-    // Determines if the NFA accepts a given input string
+     /**
+     * Determines if the NFA accepts a given input string
+     * @param input the input string to check
+     * @return true if the NFA accepts the input string, otherwise false
+     */
     @Override
     public boolean accepts(String input) {
         if (startState == null) return false;
@@ -154,7 +204,11 @@ public class NFA implements fa.nfa.NFAInterface {
         return false;
     }
 
-    // Determines the maximum number of copies an input string generates
+    /**
+     * Determines the maximum number of copies an input string generates
+     * @param input the input string to check
+     * @return the maximum number of NFA copies created while processing the input string
+     */
     @Override
     public int maxCopies(String input) {
         if (startState == null) return 1;
@@ -177,7 +231,10 @@ public class NFA implements fa.nfa.NFAInterface {
         return maxCopies;
     }
 
-    // Determines whether the NFA is actually a DFA
+    /**
+     * Determines whether the NFA is actually a DFA
+     * @return true if the NFA's transition function has DFA properties/doesn't break DFA, otherwise false
+     */
     @Override
     public boolean isDFA() {
         for (NFAState state : states) {
--- fa/nfa/NFAState.java
+++ fa/nfa/NFAState.java
@@ -1,10 +1,14 @@
 package fa.nfa;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Represents a state in an NFA.
  * Extends the abstract class State.
+ * Provides logic for transtiion maps, and adding/getting transitions.
  * Author: Parker and Sabastian
  */
 public class NFAState extends fa.State {
@@ -17,22 +21,37 @@ public class NFAState extends fa.State {
         this.transitions = new HashMap<>(); // Initialize the transition map
     }
 
-    // Adds a transition from this state to another state on the given symbol
+    /**
+     * Adds a transition from this state to another state on the given symbol
+     * @param symbol the symbol where the transition occurs
+     * @param destination the state to which the transition leads
+     */
     public void addTransition(char symbol, NFAState destination) {
         transitions.computeIfAbsent(symbol, k -> new HashSet<>()).add(destination);
     }
 
-    // Retrieves the set of states reachable from this state on a given symbol
+    /**
+     * Retrieves the set of states reachable from this state on a given symbol
+     * @param symbol the symbol where the transitions occur
+     * @return a set of states reachable from this state on the given symbol
+     */
     public Set<NFAState> getTransitions(char symbol) {
         return transitions.getOrDefault(symbol, new HashSet<>());
     }
 
-    // Retrieves the transition map of this state
+    /**
+     * Retrieves the transition map of this state
+     * @return the transition map where each symbol is a set of destination states
+     */
     public Map<Character, Set<NFAState>> getAllTransitions() {
         return transitions;
     }
 
-    // Retrieves the set of states reachable from this state on a given symbol
+    /**
+     * Retrieves the set of states reachable from X state on the given symbol
+     * @param symbol the symbol on which the transitions occur
+     * @return a set of states reachable from this state on the given symbol
+     */
     public Set<NFAState> toStates(char symbol) {
         return transitions.getOrDefault(symbol, new HashSet<>());
     }
